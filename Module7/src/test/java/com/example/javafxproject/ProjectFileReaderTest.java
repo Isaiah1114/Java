e4d9a09e1c05a3ad817436a4aa32c16bb455b9ca
icesario2 <84882338+icesario2@users.noreply.github.com>
2023-06-22 02:20:55 -0500

    Module7

---
+++ Module7/src/test/java/com/example/javafxproject/ProjectFileReaderTest.java
@@ -0,0 +1,15 @@
+package com.example.javafxproject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectFileReaderTest {
+
+    @Test
+    void wordCounter() {
+    }
+
+
+}
---
+++ Module7/src/test/java/com/example/javafxproject/WordCountApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.javafxproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordCountApplicationTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void main() {
+    }
+}
